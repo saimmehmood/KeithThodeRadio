ff6f5ed53e455e11d262dfd65e38dd4926a79400
Saim Mehmood <bsef12m557@pucit.edu.pk>
2017-10-05 23:05:50 +0500

    Adding folders

---
+++ js/audioAssets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Audio Source - Keith Thode Radio : https://wtlr.org/live-stream
+var audioData = [
+    {
+        'title' : 'Play Way Truth Life Radio',
+        'url' : 'https://wtlr.org/live-stream'
+    }
+];
+
+module.exports = audioData;
